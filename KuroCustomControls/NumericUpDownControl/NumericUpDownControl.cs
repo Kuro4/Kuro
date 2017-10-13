ea4c6d2c594ae5cc1a25180a6f219383d8a4de01
okada <yoyh9ab@outlook.jp>
2017-10-13 14:34:39 +0900

    カスタムコントロールライブラリを作る準備をしたよ

---
+++ KuroCustomControls/NumericUpDownControl/NumericUpDownControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KuroCustomControls
+{
+    public class NumericUpDownControl : Control
+    {
+        static NumericUpDownControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NumericUpDownControl),
+                new FrameworkPropertyMetadata(typeof(NumericUpDownControl)));
+        }
+    }
+}
